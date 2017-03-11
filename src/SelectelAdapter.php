09f3327b3d5023d8003c31141b6f19ff1897b6f4
tzurbaev <zurbaev@gmail.com>
2017-03-11 17:17:57 +0800

    Init

---
+++ src/SelectelAdapter.php
@@ -0,0 +1,295 @@
+<?php
+
+namespace ArgentCrusade\Flysystem\Selectel;
+
+use League\Flysystem\Config;
+use League\Flysystem\AdapterInterface;
+use ArgentCrusade\Selectel\CloudStorage\Contracts\ContainerContract;
+use ArgentCrusade\Selectel\CloudStorage\Exceptions\FileNotFoundException;
+use ArgentCrusade\Selectel\CloudStorage\Exceptions\UploadFailedException;
+use ArgentCrusade\Selectel\CloudStorage\Exceptions\ApiRequestFailedException;
+
+class SelectelAdapter implements AdapterInterface
+{
+    /**
+     * Storage container.
+     *
+     * @var \ArgentCrusade\Selectel\CloudStorage\Contracts\ContainerContract
+     */
+    protected $container;
+
+    /**
+     * Container visibility.
+     *
+     * @var string
+     */
+    protected $visibility = 'public';
+
+    /**
+     * Create new instance.
+     *
+     * @param \ArgentCrusade\Selectel\CloudStorage\Contracts\ContainerContract $container
+     */
+    public function __construct(ContainerContract $container)
+    {
+        $this->container = $container;
+        $this->visibility = $container->type() == 'gallery' ? 'public' : $container->type();
+    }
+
+    /**
+     * Loads file from container.
+     *
+     * @param string $path Path to file.
+     *
+     * @return \ArgentCrusade\Selectel\CloudStorage\Contracts\FileContract
+     */
+    protected function getFile($path)
+    {
+        return $this->container->files()->find($path);
+    }
+
+    /**
+     * Transforms internal files array to Flysystem-compatible one.
+     *
+     * @param array $files Original Selectel's files array.
+     *
+     * @return array
+     */
+    protected function transformFiles($files)
+    {
+        $result = [];
+
+        foreach ($files as $file) {
+            $result[] = [
+                'type' => 'file',
+                'path' => $file['name'],
+                'timestamp' => strtotime($file['last_modified']),
+                'visibility' => $this->visibility,
+            ];
+        }
+
+        return $result;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function has($path)
+    {
+        return $this->container->files()->exists($path);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function read($path)
+    {
+        try {
+            $file = $this->getFile($path);
+        } catch (FileNotFoundException $e) {
+            return false;
+        }
+
+        $contents = $file->read();
+
+        return compact('contents');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function readStream($path)
+    {
+        try {
+            $file = $this->getFile($path);
+        } catch (FileNotFoundException $e) {
+            return false;
+        }
+
+        $stream = $file->readStream();
+
+        rewind($stream);
+
+        return compact('stream');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function listContents($directory = '', $recursive = false)
+    {
+        $files = $this->container->files()->fromDirectory($directory)->get();
+        $result = $this->transformFiles($files);
+
+        return $result;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getMetadata($path)
+    {
+        $files = $this->listContents($path);
+
+        return isset($files[0]) ? $files[0] : false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSize($path)
+    {
+        return $this->getMetadata($path);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getMimetype($path)
+    {
+        return $this->getMetadata($path);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTimestamp($path)
+    {
+        return $this->getMetadata($path);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getVisibility($path)
+    {
+        return $this->visibility;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function write($path, $contents, Config $config)
+    {
+        try {
+            $this->container->uploadFromString($path, $contents);
+        } catch (UploadFailedException $e) {
+            return false;
+        }
+
+        return $this->getMetadata($path);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function writeStream($path, $resource, Config $config)
+    {
+        try {
+            $this->container->uploadFromStream($path, $resource);
+        } catch (UploadFailedException $e) {
+            return false;
+        }
+
+        return $this->getMetadata($path);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function update($path, $contents, Config $config)
+    {
+        return $this->write($path, $contents, $config);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function updateStream($path, $resource, Config $config)
+    {
+        return $this->writeStream($path, $resource, $config);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rename($path, $newpath)
+    {
+        try {
+            $this->getFile($path)->rename($newpath);
+        } catch (ApiRequestFailedException $e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function copy($path, $newpath)
+    {
+        try {
+            $this->getFile($path)->copy($newpath);
+        } catch (ApiRequestFailedException $e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function delete($path)
+    {
+        try {
+            $this->getFile($path)->delete($path);
+        } catch (ApiRequestFailedException $e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function deleteDir($path)
+    {
+        try {
+            $this->container->deleteDir($path);
+        } catch (ApiRequestFailedException $e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function createDir($dirname, Config $config)
+    {
+        try {
+            $this->container->createDir($dirname);
+        } catch (ApiRequestFailedException $e) {
+            return false;
+        }
+
+        return $this->getMetadata($dirname);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setVisibility($path, $visibility)
+    {
+        if ($this->visibility != $visibility) {
+            $this->visibility = $visibility;
+            $this->container->setType($visibility);
+        }
+
+        return $this->getMetadata($path);
+    }
+}
---
+++ tests/SelecteAdapterTest.php
@@ -0,0 +1,239 @@
+<?php
+
+use ArgentCrusade\Flysystem\Selectel\SelectelAdapter;
+use ArgentCrusade\Selectel\CloudStorage\Collections\Collection;
+use League\Flysystem\Config;
+
+class SelectelAdapterTest extends PHPUnit_Framework_TestCase
+{
+    public function tearDown()
+    {
+        Mockery::close();
+    }
+
+    public function selectelProvider()
+    {
+        $collection = new Collection([
+            ['name' => 'path/to/file', 'last_modified' => '2000-01-01 00:00:00'],
+        ]);
+
+        $files = Mockery::mock('ArgentCrusade\Selectel\CloudStorage\FluentFilesLoader');
+        $files->shouldReceive('fromDirectory')->andReturn($files);
+        $files->shouldReceive('get')->andReturn($collection);
+
+        $mock = Mockery::mock('ArgentCrusade\Selectel\CloudStorage\Container');
+        $mock->shouldReceive('type')->andReturn('public');
+        $mock->shouldReceive('files')->andReturn($files);
+
+        return [
+            [new SelectelAdapter($mock), $mock, $files, $collection],
+        ];
+    }
+
+    public function metaDataProvider()
+    {
+        $collection = new Collection([
+            ['name' => 'path/to/file', 'last_modified' => '2000-01-01 00:00:00'],
+        ]);
+
+        $files = Mockery::mock('ArgentCrusade\Selectel\CloudStorage\FluentFilesLoader');
+        $files->shouldReceive('fromDirectory')->andReturn($files);
+        $files->shouldReceive('get')->andReturn($collection);
+
+        $mock = Mockery::mock('ArgentCrusade\Selectel\CloudStorage\Container');
+        $mock->shouldReceive('type')->andReturn('public');
+        $mock->shouldReceive('files')->andReturn($files);
+
+        $adapter = new SelectelAdapter($mock);
+
+        return [
+            [
+                'method' => 'getMetadata',
+                'adapter' => $adapter,
+            ],
+            [
+                'method' => 'getMimetype',
+                'adapter' => $adapter,
+            ],
+            [
+                'method' => 'getTimestamp',
+                'adapter' => $adapter,
+            ],
+            [
+                'method' => 'getSize',
+                'adapter' => $adapter,
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider metaDataProvider
+     */
+    public function testMetaData($method, $adapter)
+    {
+        $result = $adapter->{$method}('path');
+
+        $this->assertInternalType('array', $result);
+    }
+
+    /**
+     * @dataProvider selectelProvider
+     */
+    public function testHas($adapter, $mock, $files)
+    {
+        $files->shouldReceive('exists')->andReturn(true);
+
+        $this->assertTrue($adapter->has('something'));
+    }
+
+    /**
+     * @dataProvider selectelProvider
+     */
+    public function testWrite($adapter, $mock)
+    {
+        $mock->shouldReceive('uploadFromString')->andReturn(md5('test'));
+
+        $result = $adapter->write('something', 'contents', new Config());
+        $this->assertInternalType('array', $result);
+        $this->assertArrayHasKey('type', $result);
+        $this->assertEquals('file', $result['type']);
+    }
+
+    /**
+     * @dataProvider selectelProvider
+     */
+    public function testUpdate($adapter, $mock)
+    {
+        $mock->shouldReceive('uploadFromString')->andReturn(md5('test'));
+
+        $result = $adapter->update('something', 'contents', new Config());
+        $this->assertInternalType('array', $result);
+        $this->assertArrayHasKey('type', $result);
+        $this->assertEquals('file', $result['type']);
+    }
+
+    /**
+     * @dataProvider selectelProvider
+     */
+    public function testWriteStream($adapter, $mock)
+    {
+        $mock->shouldReceive('uploadFromStream')->andReturn(md5('test'));
+
+        $file = tmpfile();
+        $result = $adapter->writeStream('something', $file, new Config());
+        $this->assertInternalType('array', $result);
+        $this->assertArrayHasKey('type', $result);
+        $this->assertEquals('file', $result['type']);
+        fclose($file);
+    }
+
+    /**
+     * @dataProvider selectelProvider
+     */
+    public function testUpdateStream($adapter, $mock)
+    {
+        $mock->shouldReceive('uploadFromStream')->andReturn(md5('test'));
+
+        $file = tmpfile();
+        $result = $adapter->updateStream('something', $file, new Config());
+        $this->assertInternalType('array', $result);
+        $this->assertArrayHasKey('type', $result);
+        $this->assertEquals('file', $result['type']);
+        fclose($file);
+    }
+
+    /**
+     * @dataProvider selectelProvider
+     */
+    public function testRead($adapter, $mock, $files)
+    {
+        $file = Mockery::mock('ArgentCrusade\Selectel\CloudStorage\File');
+        $file->shouldReceive('read')->andReturn('something');
+        $files->shouldReceive('find')->andReturn($file);
+
+        $result = $adapter->read('something');
+        $this->assertInternalType('array', $result);
+        $this->assertArrayHasKey('contents', $result);
+    }
+
+    /**
+     * @dataProvider selectelProvider
+     */
+    public function testReadStream($adapter, $mock, $files)
+    {
+        $stream = tmpfile();
+        fwrite($stream, 'something');
+
+        $file = Mockery::mock('ArgentCrusade\Selectel\CloudStorage\File');
+        $file->shouldReceive('readStream')->andReturn($stream);
+        $files->shouldReceive('find')->andReturn($file);
+
+        $result = $adapter->readStream('something');
+        $this->assertInternalType('array', $result);
+        $this->assertArrayHasKey('stream', $result);
+        $this->assertEquals('something', fread($result['stream'], 1024));
+
+        fclose($stream);
+    }
+
+    /**
+     * @dataProvider selectelProvider
+     */
+    public function testRename($adapter, $mock, $files)
+    {
+        $file = Mockery::mock('ArgentCrusade\Selectel\CloudStorage\File');
+        $file->shouldReceive('rename')->andReturn('newpath');
+        $files->shouldReceive('find')->andReturn($file);
+
+        $this->assertTrue($adapter->rename('oldpath', 'newpath'));
+    }
+
+    /**
+     * @dataProvider selectelProvider
+     */
+    public function testCopy($adapter, $mock, $files)
+    {
+        $file = Mockery::mock('ArgentCrusade\Selectel\CloudStorage\File');
+        $file->shouldReceive('copy')->andReturn('newpath');
+        $files->shouldReceive('find')->andReturn($file);
+
+        $this->assertTrue($adapter->copy('from', 'to'));
+    }
+
+    /**
+     * @dataProvider selectelProvider
+     */
+    public function testCreateDir($adapter, $mock)
+    {
+        $mock->shouldReceive('createDir')->andReturn(md5('test'));
+        $result = $adapter->createDir('something', new Config());
+
+        $this->assertInternalType('array', $result);
+    }
+
+    /**
+     * @dataProvider selectelProvider
+     */
+    public function testSetVisibility($adapter, $mock)
+    {
+        $mock->shouldReceive('setType')->andReturn('private');
+        $result = $adapter->setVisibility('something', 'private');
+
+        $this->assertInternalType('array', $result);
+    }
+
+    /**
+     * @dataProvider selectelProvider
+     */
+    public function testDelete($adapter, $mock, $files)
+    {
+        $file = Mockery::mock('ArgentCrusade\Selectel\CloudStorage\File');
+        $file->shouldReceive('delete')->andReturn(true);
+        $files->shouldReceive('find')->andReturn($file);
+
+        $mock->shouldReceive('deleteDir')->andReturn(true);
+
+        $this->assertTrue($adapter->delete('something'));
+        $this->assertTrue($adapter->deleteDir('something'));
+    }
+}
